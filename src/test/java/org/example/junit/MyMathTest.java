5ab89c75f7e9812387db52c4ecf440384f949b2f
seannxh <nohsean6@gmail.com>
2025-04-28 18:08:17 -0500

    ran tests on Junit and refractored

--- src/test/java/org/example/junit/MyMathTest.java
+++ src/test/java/org/example/junit/MyMathTest.java
@@ -2,11 +2,33 @@ package org.example.junit;
 
 import org.junit.jupiter.api.Test;
 
-class MyMathTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class MyMathTest {
+    private MyMath math;
     @Test
-    void testSum(){
+    void testSum3Array(){
         //Absence of failure == Success
+        //Refractored
+        int result = math.calculateSum(new int[] {1, 2, 3});
+        //System.out.println(result);
+        assertEquals(6, result);
+
+
+
+    }
+    @Test
+    void testSum2EmptyArray(){
+        //Absence of failure == Success
+
+        int[] numbers = {};
+        MyMath math = new MyMath();
+        int result = math.calculateSum(numbers);
+        //System.out.println(result);
+        int ExpectedResult = 0;
+        assertEquals(result, ExpectedResult);
+
+
 
     }
 
